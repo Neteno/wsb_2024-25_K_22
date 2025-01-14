7114dafa91a27f9fc947d65ec60e3eb185a75cff
Neteno <szymon.andrzejczak@spoko.pl>
2025-01-14 18:24:10 +0100

    delegaty_3

---
+++ 5_3_delegaty/scr/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using _5_3_delegaty.scr.enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static _5_3_delegaty.Program;
+
+namespace _5_3_delegaty.scr.Models
+{
+    internal class User
+    {
+        public string Username { get; set; }
+        public List<Role> Roles { get; set; }
+
+        public User(string username)
+        {
+            Username = username;
+            Roles = new List<Role>();
+        }
+
+        public void AddRole(Role role)
+        {
+            if (!Roles.Contains(role))
+            {
+                Roles.Add(role);
+            }
+        }
+    }
+}
